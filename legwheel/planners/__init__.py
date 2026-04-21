e249aba20262dcb307b97288bd044cffbb43dbae
Starlee-0514 <starlee0514@gmail.com>
2026-04-21 15:38:15 +0800

    feat(Com):add com stability & testing script

--- legwheel/planners/__init__.py
+++ legwheel/planners/__init__.py
@@ -4,5 +4,12 @@ from legwheel.planners.trajectory_planning import TrajectoryPlanner
 from legwheel.planners.gait_generator import Gait_Generator
 from legwheel.planners.trajectory_planning_3d import TrajectoryPlanner3D
 from legwheel.planners.gait_generator_3d import GaitGenerator3D
+from legwheel.planners.com_stability import COMStabilityPlanner
 
-__all__ = ["TrajectoryPlanner", "Gait_Generator", "TrajectoryPlanner3D", "GaitGenerator3D"]
+__all__ = [
+    "TrajectoryPlanner",
+    "Gait_Generator",
+    "TrajectoryPlanner3D",
+    "GaitGenerator3D",
+    "COMStabilityPlanner",
+]
